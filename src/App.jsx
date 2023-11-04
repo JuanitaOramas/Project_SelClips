3bc900e493158cde5d5852ff08dabf16c498bd76
Gabriela Castro Santamaría <gabycs031002@gmail.com>
2023-11-03 20:58:35 -0500

    iconos menu y cambios app :smile:

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Loginpage from './pages/Loginpage';
 import Menu from './components/Menu';
 import Threadpage from './pages/Threadpage';
 import SpecificThreadpage from './pages/SpecificThreadpage';
+import Profilepage from './pages/Profilepage';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
@@ -15,17 +16,21 @@ function App() {
       <div className="App">
         <div>
           <Row>
-            <Col style={{ backgroundColor: 'rgb(238,221,207)' }} md={3} sm={3}>
-              <Menu />
-              {/* <Routes>
+            <Col style={{ backgroundColor: 'rgb(238,221,207)' }} md={2} sm={2}>
+              {/*<Menu />*/}
+               <Routes>
                 <Route path="/" element={<Menu />} />
-              </Routes> */}
+                <Route path="/threads" element={<Menu />} />
+                <Route path="/threads/:id" element={<Menu />} />
+                <Route path="/account" element={<Menu />} />
+              </Routes>
             </Col>
-            <Col md={9} sm={9}>
+            <Col md={10} sm={10}>
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/threads" element={<Threadpage />} />
                 <Route path="/threads/:id" element={<SpecificThreadpage />} />
+                <Route path="/account" element={<Profilepage />} />
               </Routes>
             </Col>
             <Col md={12} >
