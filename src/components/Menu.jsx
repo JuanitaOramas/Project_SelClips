8cfbcb1a7098c43213de5b33bcaa807849b2f9f6
Gabriela Castro Santamaría <gabycs031002@gmail.com>
2023-11-03 20:15:55 -0500

    changes in app and menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,17 +7,17 @@ import logo from "../logoSelfClips.png";
 
 function VerticalMenu() {
   return (
-    <div className="page-container">
+
       <Navbar className="sidebar" bg="body-tertiary">
         <img src={logo} alt="Logo" className="logo" />
         <Nav className="flex-column">
-          <Link to="/" className="text-dark">Home</Link>
+          <Link to="/" className="text-dark"><i class="bi bi-house">Home</i></Link>
           <Link to="/login" className="text-dark">Login</Link>
           <Link to="/account" className="text-dark">Account</Link>
           <Link to="/threads" className="text-dark">Thread</Link>
         </Nav>
       </Navbar>
-    </div>
+
   );
 }
 
